ff29725cf944571ec18ca4886bf4dfe578c8e4de
Zeeshan Syed <zee@hakstudio.com>
2013-09-09 22:51:09 -0400

    Fixed static stuff

--- mycleancity/urls.py
+++ mycleancity/urls.py
@@ -6,18 +6,19 @@ from django.views.generic import TemplateView
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	# url(r'^$', 'mycleancity.views.index', name='home'),
-    url(r'^$', TemplateView.as_view(template_name="index.html")),
+	# url(r'^test/?', 'mycleancity.views.index', name='test'),
 
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+	url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+	url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^challenges/', include('challenges.urls')),
-    url(r'^cleancreds/', include('cleancreds.urls')),
-    url(r'^users/', include('users.urls')),
+	url(r'^challenges/', include('challenges.urls')),
+	url(r'^cleancreds/', include('cleancreds.urls')),
+	url(r'^users/', include('users.urls')),
+
+	url(r'^$', TemplateView.as_view(template_name="index.html")),
 )
 
 if settings.DEBUG:
-    urlpatterns += patterns('django.contrib.staticfiles.views',
-        url(r'^static/(?P<path>.*)$', 'serve'),
-    )
+	urlpatterns += patterns('django.contrib.staticfiles.views',
+		url(r'^static/(?P<path>.*)$', 'serve'),
+	)
