e288f1ef2d43c9b7bbe76c491278163e9d334357
Zeeshan Syed <zee@hakstudio.com>
2013-09-17 12:51:11 -0400

    Added admin for prelaunchemails

--- users/admin.py
+++ users/admin.py
@@ -5,4 +5,9 @@ class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'organization', 'dob', 'city', 'postal_code', 'country')
     search_fields = ['user__id', 'user__first_name', 'user__last_name']
 
+class PrelaunchEmailsAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'email', 'postal_code', 'school_type', 'ambassador', 'timestamp')
+    search_fields = ['email', 'first_name', 'school_type']
+
 admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(PrelaunchEmails, PrelaunchEmailsAdmin)
