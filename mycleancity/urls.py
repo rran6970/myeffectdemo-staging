75eff9c9a34c878303bcd3f9b6fd619405c00a69
Zeeshan Syed <zee@hakstudio.com>
2013-10-04 18:46:38 -0400

    Fixed admin issue

--- mycleancity/urls.py
+++ mycleancity/urls.py
@@ -9,11 +9,8 @@ from users.views import PrelaunchView
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	# url(r'^test/?', 'mycleancity.views.index', name='test'),
-
 	url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 	url(r'^admin/', include(admin.site.urls)),
-	# url(r'^accounts/login/?', 'users.views.login'),
 
 	url(r'^challenges/', include('challenges.urls')),
 	url(r'^cleancreds/', include('cleancreds.urls')),
