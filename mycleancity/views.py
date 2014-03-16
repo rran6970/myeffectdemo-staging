212488b127f40e77210701c51bd9c135ad003fdb
Zeeshan Syed <zee@hakstudio.com>
2014-03-16 11:49:45 -0400

    Added French navbar changes and French welcome package

--- mycleancity/views.py
+++ mycleancity/views.py
@@ -83,11 +83,13 @@ def download_file(request):
 	k = bucket.get_key(filename)
 	url = k.generate_url(6000)
 
-	if filename == "downloadable/Welcome_package_cgd.pdf":
-		if request.user.profile.clean_team_member:
-			if request.user.profile.clean_team_member.clean_team.level.name == "Seedling":
-				task = CleanTeamLevelTask.objects.get(name="download_welcome_package")
-				request.user.profile.clean_team_member.clean_team.complete_level_task(task)
+	if request.user.is_active:
+		if filename == "MCC_welcome_package_FR.pdf" or filename == "Welcome_package_cgd":
+			if request.user.profile.clean_team_member:
+				if request.user.profile.clean_team_member.clean_team.level.name == "Seedling":
+					task = CleanTeamLevelTask.objects.get(name="download_welcome_package")
+					request.user.profile.clean_team_member.clean_team.complete_level_task(task)
+
 
 	# TODO: Fix this so there is a redirect, as well as a link to download
 	# return HttpResponseRedirect('/clean-team/level-progress/')
