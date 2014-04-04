da1230f4afc505aa13bba8693463609e4e93e709
balakrishnan <balakrishnan@gamillc.com>
2014-04-04 18:18:21 +0530

    new api updated by Edwin

--- api/api.py
+++ api/api.py
@@ -19,7 +19,7 @@ from django.template.loader import get_template
 from django.template import Context, RequestContext
 from django.core.mail import EmailMessage
 from userprofile.models import UserProfile, QRCodeSignups, UserQRCode,UserSettings
-from challenges.models import Challenge, UserChallenge, ChallengeQRCode
+from challenges.models import Challenge, UserChallenge, ChallengeCategory, ChallengeQRCode
 from cleanteams.models import CleanTeam, CleanTeamMember, CleanTeamPost, CleanChampion, CleanTeamInvite, CleanTeamLevel
 from notifications.models import UserNotification
 import json,urlparse,random,string, base64,datetime
@@ -53,6 +53,7 @@ def login_user(request):
 			"""
 			role=""
 			cleanteamname =""
+			cleanteamid=""
 			try:
 				#role = user.cleanteammember_set.values_list('role',flat=True).get()
 				role_array    = CleanTeamMember.objects.get(user_id=user.id,status="approved")
@@ -576,7 +577,8 @@ def my_challenge(request):
 			,'time_in':str(each.time_in)
 			,'time_out':str(each.time_out)
 			,'type_id':challengearray.type_id
-			,'qr_code':qrimage})	
+			,'qr_code':qrimage
+			,'last_updated_by_id':challengearray.last_updated_by_id})	
 		response_base.response['status'] = 1
 		response_base.response['teamstatus'] = 0
 		response_base.response['data'] = jsonvalue	
