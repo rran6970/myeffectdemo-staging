b751c3d971a7c946d6c12093e6270615f2db02a8
Zeeshan Syed <zee@hakstudio.com>
2014-04-04 18:04:28 -0400

    Added read all notifications

--- notifications/urls.py
+++ notifications/urls.py
@@ -5,6 +5,7 @@ from notifications.views import *
 
 urlpatterns = patterns('',
 	url(r'^/?$', NotificationsFeedView.as_view()),
+	url(r'^read-all/?$', 'notifications.views.read_all_notifications'),
 	url(r'^read/?$', 'notifications.views.read_notification'),
 	url(r'^quick-read/?$', 'notifications.views.read_notification'),
 	url(r'^quick-unread/?$', 'notifications.views.unread_notification'),
