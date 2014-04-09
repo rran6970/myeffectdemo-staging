282a91741e9b59e0582e5487397734bfc8bd6d65
edwin <edwin@matrida.co.in>
2014-04-09 10:26:56 +0530

    New api pushed

--- api/api.py
+++ api/api.py
@@ -170,6 +170,7 @@ def student_edit_profile(request):
 	user = User.objects.get(id=request_obj.params['userid'])
 	role=""
 	cleanteamname =""
+	cleanteamid=""
 	try:
 		#role = user.cleanteammember_set.values_list('role',flat=True).get()
 		role_array    = CleanTeamMember.objects.get(user_id=request_obj.params['userid'],status="approved")
@@ -181,13 +182,14 @@ def student_edit_profile(request):
 			champarray = CleanChampion.objects.get(user_id=request_obj.params['userid'])
 			role = "clean-champion"
 		except Exception,e:
-			role = ""
+			print e
+			role = "Individual"
 		
 	try:
 		cleanteamid  = user.cleanteammember_set.values_list('clean_team_id',flat=True).get()
 		cleanteamArray  = CleanTeam.objects.get(id=cleanteamid)
 		cleanteamname  = cleanteamArray.name
-	except exception,e:
+	except Exception,e:
 		cleanteamname =""
 	#print cleanteamname
 	#print user_profile.picture
@@ -545,10 +547,14 @@ def my_challenge(request):
 	response_base = ResponseDic()
 	userid = request_obj.params['userid']
 	try:
-		challenge = UserChallenge.objects.filter(user_id=userid)
+		Uchallenge = UserChallenge.objects.filter(user_id=userid)
 		jsonvalue =[]
-		for each in challenge:		
+		for each in Uchallenge:		
 			challengeid = each.challenge_id
+			ChallengesInstance  = each.challenge
+			CTeam  = ChallengesInstance.clean_team
+			ctname     = CTeam.name
+			#print ctname
 			challengearray  = Challenge.objects.get(id=challengeid)
 			qr_id = challengearray.qr_code_id
 			if qr_id:
@@ -570,6 +576,7 @@ def my_challenge(request):
 			,'address1':challengearray.address1
 			,'address2':challengearray.address2
 			,'city':challengearray.city
+			,'ctname':ctname
 			,'province':challengearray.province
 			,'postal_code':challengearray.postal_code
 			,'country':challengearray.country
@@ -725,6 +732,9 @@ def search(request):
 			,'ctname':ctname
 			,'title':each.title
 			,'org':each.host_organization
+			,'address1':each.address1
+			,'address2':each.address2
+			,'postal_code':each.postal_code
 			,'city':each.city
 			,'eventdate':str(each.event_date)
 			,'eventtime':str(each.event_time)
