edd7ba07d70451092fb3b3efed633bc2eb7ac7e8
Zeeshan Syed <zee@hakstudio.com>
2014-04-14 19:17:36 -0400

    Created URL tracking for signs

--- mycleancity/static/js/scripts.js
+++ mycleancity/static/js/scripts.js
@@ -1,5 +1,17 @@
 $(function(){  
     $(".btn").button();
+
+    // Javascript to enable link to tab
+    var url = document.location.toString();
+    if (url.match('#')) {
+        $('.nav-tabs a[href=#'+url.split('#')[1]+']').tab('show') ;
+    } 
+
+    // Change hash for page-reload
+    $('.nav-tabs a').on('shown', function (e) {
+        window.location.hash = e.target.hash;
+    })
+
 	$(".dropdown-toggle").dropdown();
 	$(".timepicker").timepicker({
         'defaultTime': false,
