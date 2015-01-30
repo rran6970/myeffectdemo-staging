a7b9c54f4f67dd5f2c2c214ec84da3647433d5f8
Nathan <jarlaxe227@gmail.com>
2015-01-30 10:32:43 -0800

    changed settings.py to have the database connect properly

--- mycleancity/settings.py
+++ mycleancity/settings.py
@@ -16,7 +16,7 @@ DATABASES = {
         'NAME': 'mycleancity',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'root',
-        'PASSWORD': 'another123',
+        'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                     # Set to empty string for default.
         'OPTIONS': {
@@ -172,7 +172,6 @@ INSTALLED_APPS = (
     'django_mobile',
     'django_wysiwyg',
     "django_crontab",
-    'django_mailer',
     'captcha',
     'cleancreds',
     'cleanteams',
@@ -224,7 +223,7 @@ LOGGING = {
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-# DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
