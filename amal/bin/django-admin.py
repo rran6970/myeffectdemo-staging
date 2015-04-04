d98f8057199a7d89814eac166d864df9f04ef64a
Robert Elder <robert@robertelder.org>
2015-04-04 17:53:52 +0000

    Remove unnecessary files and changes.

--- amal/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/amal/Downloads/Python-2.7.9/myeffect/my-effect-main/amal/bin/python2.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
