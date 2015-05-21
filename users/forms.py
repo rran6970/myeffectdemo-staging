33b236c3c47ecb2bc7997081d50fb7261a9c933f
nishant gupta <nishant.9488@gmail.com>
2015-05-21 14:59:20 -0400

    platform glitches resolved

--- users/forms.py
+++ users/forms.py
@@ -352,7 +352,7 @@ class SettingsForm(forms.ModelForm):
     communication_language = forms.ChoiceField(widget=forms.RadioSelect, choices=COMM_CHOICES, label="Communication language")
     email_privacy = forms.ChoiceField(widget=forms.RadioSelect, choices=YES_NO_CHOICES, label="Hide My email from my public profile?")
     from_privacy = forms.ChoiceField(widget=forms.RadioSelect, choices=YES_NO_CHOICES, label="Hide My location details from my public profile?")
-    data_privacy = forms.ChoiceField(widget=forms.RadioSelect, choices=YES_NO_CHOICES, label="I consent to share my volunteer data with the following Change Teams & Communities.")
+    data_privacy = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple(attrs={"checked":""}), label="I consent to share my volunteer data with the following Change Teams & Communities.", required=False)
     receive_newsletters = forms.ChoiceField(widget=forms.RadioSelect, choices=YES_NO_CHOICES, label="Receive My Effect email communications")
     timezone = forms.ChoiceField(widget=forms.Select(), choices=SCHOOLS, label="Select your timezone (default is America/Toronto)")
 
